988bc2d36df7a82909f163298b93631387db83b8
Christoph Walcher <Christoph.Walcher@gmail.com>
2012-03-11 19:12:17 +0100

    Adds post processors

--- bin/remarkedup.js
+++ bin/remarkedup.js
@@ -1,23 +1,26 @@
 #!/usr/bin/env node
 
-var commander = require('commander'),
-    batchGenerate = require('../lib/batch-generate.js');
+var program = require('commander'),
+    batchGenerate = require('../lib/batch-generate.js'),
+    processToc = require('../lib/process-toc.js');
 
-commander.version('0.1.0')
+program.version('0.1.0')
     .description('parses markdown files and transforms them to html.')
     .option('-i, --input <glob>', 'input file or path')
     .option('-o, --output <path>', 'output path')
     .option('-t, --template', 'html template file to use')
     .option('-w, --watch', 'watch files for modification and regenerate html output');
 
-commander.parse(process.argv);
+program.parse(process.argv);
 
 // Get extra args passed to commander
-for (var i = 0; i < commander.args.length; i++) {
-    var parts = commander.args[i].split('=');
+for (var i = 0; i < program.args.length; i++) {
+    var parts = program.args[i].split('=');
     if (parts && parts.length == 2) {
-        commander[parts[0]] = parts[1];
+        program[parts[0]] = parts[1];
     }
 }
 
-batchGenerate(commander);
+program.postProcessors = [];
+program.postProcessors.push(processToc);
+batchGenerate(program);
